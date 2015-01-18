fe696b8fa8d936ff997163af58ab33188e43e4ad
Nosfe7 <gianicolo89@gmail.com>
2015-01-18 22:31:05 +0100

    Update Mostro.cs

--- Disturbia/Assets/Scripts/Mostro.cs
+++ Disturbia/Assets/Scripts/Mostro.cs
@@ -1,16 +1,24 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Mostro : MonoBehaviour {
 	private NavMeshAgent agent;
 	private Vector3 playerPos;
 	private Vector3 relativePos;
-	private Animator animator;
-	//Vector3 previous_direction;
+	private ArrayList animations;
+	private AnimationClip idle;
+	private AnimationClip walking;
+
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
-		animator = GetComponent<Animator> ();
+		animations = new ArrayList ();
+
+		foreach(AnimationState state in animation)
+		{
+			Debug.Log(state.name);
+			animations.Add(state.name);
+		}
 	}
 	
 	// Update is called once per frame
@@ -22,19 +30,21 @@ public class Mostro : MonoBehaviour {
 		agent.updateRotation = true;
 
 
-		if (agent.remainingDistance<agent.stoppingDistance) {//Se il mostro è vicino..
+		//Il mostro si gira verso il giocatore
+		relativePos = new Vector3 (transform.position.x - playerPos.x, 0.0f, transform.position.z - playerPos.z);
+		
+		Quaternion targetRotation = Quaternion.LookRotation (relativePos);
+		
+		transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRotation, Time.deltaTime*10);
+
+
+		if (agent.remainingDistance<agent.stoppingDistance && !PlayerObject.getInstance.isMoving()) {//Se il mostro è vicino e il giocatore è fermo..
 			//Il mostro è fermo
-			animator.Play("idle");
+			animation.Play((string)animations[0]);
 
-			//Il mostro si gira verso il giocatore
-			relativePos = new Vector3 (transform.position.x - playerPos.x, 0.0f, transform.position.z - playerPos.z);
+		} else if (agent.remainingDistance>=agent.stoppingDistance) {//Se invece è lontano cammina
+					animation.Play((string)animations[4]);
 
-			Quaternion targetRotation = Quaternion.LookRotation (-relativePos);
-
-			transform.localRotation = Quaternion.Slerp (transform.localRotation, targetRotation, Time.deltaTime*10);
-
-				} else {//Se invece è lontano cammina
-						animator.Play("walking");
 
 				}
 		
